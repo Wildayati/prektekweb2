a2dc3294f3b21e37838578f68cd9d4f6e9aa369c
wildayati <willda@student.untan.ac.id>
2017-05-02 13:36:13 +0700

    ralat template awal

--- administrator/template/main.php
+++ administrator/template/main.php
@@ -1,6 +1,6 @@
 <?php 
-include_once 'contend.php';
-include_once 'footer.php';
 include_once 'header.php';
-include_once 'siderbar.php';
+include_once 'contend.php';
+include_once 'sidebar.php';
+include_once 'footer.php';
  ?>
