6c89d432a1ad00911c5c5573e5aaab427e385851
wildayati <willda@student.untan.ac.id>
2017-05-24 10:01:06 +0700

    Update tanggal

--- pages/bloghome.php
+++ pages/bloghome.php
@@ -10,11 +10,8 @@
                 <p class="lead">
                     by <a href="index.php">Start Bootstrap</a>
                 </p>
-                <p><i class="fa fa-clock-o"></i> Posted on <?php echo $ib['tanggal'] ?></p>
+                <p><i class="fa fa-clock-o"></i> Posted on <?php echo date('d F Y',strtotime($ib['tanggal']));  ?></p>
                 <hr>
-                <a href="blog-post.html">
-                    <img class="img-responsive img-hover" src="http://placehold.it/900x300" alt="">
-                </a>
                 <hr>
                 <p><?php echo $ib['isi'] ?></p>
                 <a class="btn btn-primary" href="#">Read More <i class="fa fa-angle-right"></i></a>
